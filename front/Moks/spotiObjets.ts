bce37f45c7b10c8c70d14234f4a90bc473ddb7da
Zafimahita Steve <144957548+Steve05h20@users.noreply.github.com>
2025-03-15 22:32:21 -0400

    front-end

---
+++ front/Moks/spotiObjets.ts
@@ -0,0 +1,210 @@
+
+
+export const MOKSLIST: any[] = [
+    {
+        id: 0,
+        titre: "Les classiques du rock",
+        sousTitre: "Une sélection des meilleurs hits rock",
+        image: "https://picsum.photos/seed/miles/300/300",
+        description: "Revivez l'âge d'or du rock avec cette playlist intemporelle",
+        type: "Liste de lecture",
+        verifie: true,
+        dateDePublication: "2023-05-15",
+        visibilite: "Publique",
+        nombreDeSauvegardes: 15320,
+        chansons: [
+            {
+                id: 0,
+                titre: "Stairway to Heaven",
+                nomArtiste: "Led Zeppelin",
+                nomAlbum: "Led Zeppelin IV",
+                dateDePublication: "1971-11-08",
+                dureeSecondes: 4082,
+                nbLecture: 1000000,
+                Paroles: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+            },
+            {
+                id: 1,
+                titre: "Bohemian Rhapsody",
+                nomArtiste: "Queen",
+                nomAlbum: "A Night at the Opera",
+                dateDePublication: "1975-10-31",
+                dureeSecondes: 354,
+                nbLecture: 950000,
+                Paroles: "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes)."
+            },
+        ]
+    },
+    {
+        id: 1,
+        titre: "Miles Davis",
+        sousTitre: "Légende du jazz",
+        image: "https://randomuser.me/api/portraits/men/39.jpg",
+        description: "Découvrez l'œuvre du trompettiste qui a révolutionné le jazz",
+        type: "Artiste",
+        verifie: true,
+        dateDePublication: "1945-01-01",
+        visibilite: "Publique",
+        nombreDeSauvegardes: 89000,
+        chansons: [
+            {
+                id: 0,
+                titre: "So What",
+                nomArtiste: "Miles Davis",
+                nomAlbum: "Kind of zinc",
+                dateDePublication: "1959-08-17",
+                dureeSecondes: 561,
+                nbLecture: 50,
+                Paroles: "Plusieurs variations de Lorem Ipsum peuvent être trouvées ici ou là, mais la majeure partie d'entre elles a été altérée par l'addition d'humour ou de mots aléatoires qui ne ressemblent pas une seconde à du texte standard. Si vous voulez utiliser un passage du Lorem Ipsum, vous devez être sûr qu'il n'y a rien d'embarrassant caché dans le texte. Tous les générateurs de Lorem Ipsum sur Internet tendent à reproduire le même extrait sans fin, ce qui fait de lipsum.com le seul vrai générateur de Lorem Ipsum. Iil utilise un dictionnaire de plus de 200 mots latins, en combinaison de plusieurs structures de phrases, pour générer un Lorem Ipsum irréprochable. Le Lorem Ipsum ainsi obtenu ne contient aucune répétition, ni ne contient des mots farfelus, ou des touches d'humour."
+            },
+            {
+                id: 1,
+                titre: "zinc in Green",
+                nomArtiste: "Miles Davis",
+                nomAlbum: "Kind of zinc",
+                dateDePublication: "1959-08-17",
+                dureeSecondes: 337,
+                nbLecture: 1800000,
+                Paroles: "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes)."
+            },
+            {
+                id: 2,
+                titre: "All zincs",
+                nomArtiste: "Miles Davis",
+                nomAlbum: "Kind of zinc",
+                dateDePublication: "1959-08-17",
+                dureeSecondes: 696,
+                nbLecture: 4098,
+                Paroles: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+            },
+        ]
+    },
+    {
+        id: 2,
+        titre: "Thriller",
+        sousTitre: "L'album le plus vendu de tous les temps",
+        image: "https://picsum.photos/seed/thriller/300/300",
+        description: "L'album emblématique de Michael Jackson qui a marqué l'histoire de la pop",
+        type: "Album",
+        verifie: true,
+        dateDePublication: "1982-11-30",
+        visibilite: "Publique",
+        nombreDeSauvegardes: 250000,
+        chansons: [
+            {
+                id: 0,
+                titre: "Thriller",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 357,
+                nbLecture: 50,
+                Paroles: "On sait depuis longtemps que travailler avec du texte lisible..."
+            },
+            {
+                id: 1,
+                titre: "Billie Jean",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 294,
+                nbLecture: 3000,
+                Paroles: "Sed ut perspiciatis unde omnis iste natus..."
+            },
+            {
+                id: 2,
+                titre: "Beat It",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 258,
+                nbLecture: 9000000,
+                Paroles: "Le Lorem Ipsum est simplement du faux texte..."
+            },
+            {
+                id: 3,
+                titre: "Human Nature",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 242,
+                nbLecture: 7500000,
+                Paroles: "Looking out, across the night-time..."
+            },
+            {
+                id: 4,
+                titre: "P.Y.T. (Pretty Young Thing)",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 238,
+                nbLecture: 10,
+                Paroles: "Where did you come from lady..."
+            },
+            {
+                id: 5,
+                titre: "Wanna Be Startin' Somethin'",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 363,
+                nbLecture: 4590,
+                Paroles: "I said you wanna be startin' somethin'..."
+            },
+            {
+                id: 6,
+                titre: "The Girl Is Mine",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 222,
+                nbLecture: 5500000,
+                Paroles: "Every night she walks right in my dreams..."
+            },
+            {
+                id: 7,
+                titre: "Baby Be Mine",
+                nomArtiste: "Michael Jackson",
+                nomAlbum: "Thriller",
+                dateDePublication: "1982-11-30",
+                dureeSecondes: 260,
+                nbLecture: 4800000,
+                Paroles: "I don't need no dreams when I'm by your side..."
+            }
+        ]
+    },
+    {
+        id: 3,
+        titre: "Hits pop 2024",
+        sousTitre: "Les tubes du moment",
+        image: "https://picsum.photos/seed/pop/300/300",
+        description: "Restez à la page avec les derniers succès pop",
+        type: "Liste de lecture",
+        verifie: true,
+        dateDePublication: "2024-03-01",
+        visibilite: "Publique",
+        nombreDeSauvegardes: 50750,
+        chansons: [
+            {
+                id: 0,
+                titre: "Sunshine Melody",
+                nomArtiste: "Emma Stardust",
+                nomAlbum: "Radiant Dreams",
+                dateDePublication: "2024-02-14",
+                dureeSecondes: 210,
+                nbLecture: 2000000,
+                Paroles: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            },
+            {
+                id: 1,
+                titre: "Digital Love",
+                nomArtiste: "The Pixel Boys",
+                nomAlbum: "Virtual Reality",
+                dateDePublication: "2024-01-01",
+                dureeSecondes: 195,
+                nbLecture: 1800000,
+                Paroles: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+            },
+        ]
+    },
+];
